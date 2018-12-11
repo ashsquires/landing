1583dc498fabbd46e2a42406fdf5d9b11f317ba0
Ashton Squires <ashontsquires@gmail.com>
2018-12-11 08:18:48 +0000

    ani

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ btn.addEventListener('click', () => window.scrollTo({
   top: 800,
   behavior: 'smooth',
 }));
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    //document.getElementById("homenavbar").style.height = "75px";
+    document.getElementById("homenavbar").classList.add('smoothshrink');
+  } else {
+    //document.getElementById("homenavbar").style.height = "82px";
+    document.getElementById("homenavbar").classList.remove('smoothshrink');
+  }
+}
